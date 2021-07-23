2c3cccfc3a496daa4e9a93f544390b7f5180428f
AKS <AKS-coder@users.noreply.github.com>
2021-07-23 14:23:35 +0800

    Vertical aligned everything

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,23 +7,25 @@ function Home() {
     return (
         <div className="site" >
             <Navbar />
-            <div className="container text-center home_content">
+            <div className="container-fluid text-center home_content">
 
                 {/* GEEKIES title */}
-                <div className="row align-items-center ">
-                    <div className="col-md"><h1 className="title">Geekies</h1></div>
-                </div>
-
-                {/* Button */}
-                <div className="row align-items-center">
-                    <div className="col-md">
-                        <Link to="/getStarted"><button className="btn btn-lg button_home Right_btn">&emsp;Get Started&emsp;</button></Link>
+                <div>
+                    <div className="row align-items-center ">
+                        <div className="col-md"><h1 className="title">Geekies</h1></div>
                     </div>
-                </div>
 
-                {/* Subtititle */}
-                <div className="row align-items center">
-                    <div className="col-md"><h5 className="subtitle">click to find out more</h5></div>
+                    {/* Button */}
+                    <div className="row align-items-center">
+                        <div className="col-md">
+                            <Link to="/getStarted"><button className="btn btn-lg button_home Right_btn">&emsp;Get Started&emsp;</button></Link>
+                        </div>
+                    </div>
+
+                    {/* Subtititle */}
+                    <div className="row align-items center">
+                        <div className="col-md"><h5 className="subtitle">click to find out more</h5></div>
+                    </div>
                 </div>
             </div>
         </div>
