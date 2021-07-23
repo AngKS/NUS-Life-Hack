f0dd178bc74fb12c27ca8be90c59a2e5c6c12a9d
Tan Shi Wei Cody <72619037+A-Cup-Of-Coffee-Cody@users.noreply.github.com>
2021-07-23 23:14:28 +0800

    Course_Start

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Profile from './components/pages/Profile'
 import GetStarted from './components/pages/GetStarted';
+import Course from './components/pages/Course';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/getStarted" exact component={GetStarted} />
           <Route path="/profile" exact component={Profile} />
+          <Route path="/course" exact component={Course} />
         </Switch>
       </Router>
     </div>
